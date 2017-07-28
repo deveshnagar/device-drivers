5ec0244d37e6560e8533a78b63dcd0fdf3cff1ce
Devesh Nagar <devesh66@gmail.com>
2017-07-29 01:57:34 +0530

    added example for multiple minors
    Example to handle multiple minors simple and gpio leds.
    
    Signed-off-by: Devesh Nagar <devesh66@gmail.com>

---
+++ char-drivers/multiple-gpio-led.c
@@ -0,0 +1,295 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/gpio.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <asm/uaccess.h>
+#include <linux/version.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+
+#define FIRST_MINOR 1		//First minor
+#define TOTAL_MINOR 4		//Total minors
+#define GPIO_LED_1 1		//gpio number
+#define GPIO_LED_2 6		//gpio numner
+#define GPIO_LED_3 7		//gpio numner
+#define GPIO_LED_4 9		//gpio numner
+unsigned int gpio_led[] = {GPIO_LED_1, GPIO_LED_2, GPIO_LED_3, GPIO_LED_4};
+int i;
+
+/*
+ * It is not working with GPIO_NUMBER=7 !!... might be because I defined same pin in DTS file also for led class.!
+ * To make it work, I added :
+ * For init :
+ * 	gpio_is_valid(GPIO_NUMBER);
+ * 	gpio_request(GPIO_NUMBER, "GPIO_7");
+ * 	gpio_direction_output(GPIO_NUMBER, 0);
+ * For cleanup :
+ * 	gpio_free(GPIO_NUMBER);
+ * 
+ * After on doing insmod I can see :
+ * 	/sys/class/gpiodrv
+ *
+ */
+
+/*
+ * Radxa rock gpio pins
+ * GPIO0_A5 = 165 = 5	//Can  use
+ * GPIO0_A6 = 166 = 6	//Used by devesh in dts led class, check dts if not used
+ * GPIO0_A7 = 167 = 7	//Can use
+ * GPIO0_B0 = 168 = 8	//Can use
+ * GPIO0_A1 = 161 = 1	//Can use
+ * GPIO0_B1 = 169 = 9	//Can use
+ * GPIO3_D4 = 284 = 124 //Can ?
+ * GPIO3_D5 = 285 = 125
+ * GPIO1_A0 = 192 = 31
+ * GPIO1_A1 = 193 = 32
+ * GPIO1_A2 = 194 = 33
+ * GPIO1_A3 = 195 = 34
+ * For mapping others reffer:
+ * http://wiki.radxa.com/Rock/GPIO and http://wiki.radxa.com/File:Extension_header_funca.png
+ **BDFORE DOING ANY CHANGE IN GPIO pins, PLEASE REFFER ABOVE LINKS PROPERLY
+ */ 
+
+static dev_t dev_no;			//For Device number major and minor
+static struct cdev char_dev;	//For Char device struture
+static struct class *class_dev;	//Pointer to the class structure of this device
+
+static ssize_t gpio_read(struct file* file_p, char *buf, size_t count, loff_t *f_pos)
+{
+	printk(KERN_INFO "gpio_led: gpio_read from = %d:%d\n", imajor(file_p->f_inode), iminor(file_p->f_inode) );
+	unsigned int ret;
+	char temp[2];
+	int gpio_number;
+
+	switch(iminor(file_p->f_inode))
+	{
+		case FIRST_MINOR:
+			gpio_number = gpio_led[0];
+			break;
+		case 2:
+			gpio_number = gpio_led[1];
+			break;
+		case 3:
+			gpio_number = gpio_led[2];
+			break;
+		case TOTAL_MINOR:
+			gpio_number = gpio_led[3];
+			break;
+		default:
+			return -1;
+	}
+	ret = gpio_get_value(gpio_number);
+
+	temp[0] = ret + '0';
+	temp[1] = '\n';
+	printk(KERN_INFO "gpio_led: read value = %c\n", temp[0]);
+
+	ret = 2; //size of data to be written
+	if(*f_pos == 0)
+	{
+		printk(KERN_INFO "f_pos = 0\n");
+		ret = copy_to_user(buf, &temp, 2);
+		*f_pos = *f_pos + 2; //So that it will come out from cat command. Here 1 is number of bytes written.
+		return 2;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+static ssize_t gpio_write(struct file* file_p, const char *buf, size_t count, loff_t *f_pos)
+{
+	char temp;
+	unsigned int gpio_number = GPIO_LED_1;
+	printk(KERN_INFO "gpio_led: gpio_write from = %d:%d\n", imajor(file_p->f_inode), iminor(file_p->f_inode) );
+
+	if (copy_from_user(&temp, buf, count))
+	{
+		return -EFAULT;
+	}
+
+	switch(iminor(file_p->f_inode))
+	{
+		case FIRST_MINOR:
+			gpio_number = gpio_led[0];
+			break;
+		case 2:
+			gpio_number = gpio_led[1];
+			break;
+		case 3:
+			gpio_number = gpio_led[2];
+			break;
+		case TOTAL_MINOR:
+			gpio_number = gpio_led[3];
+			break;
+		default:
+			return -1;
+	}
+	switch(temp)
+	{
+		case '0':
+			gpio_set_value(gpio_number, 0);
+			break;
+		case '1':
+			gpio_set_value(gpio_number, 1);
+			break;
+		default :
+			printk(KERN_INFO "gpio_led: wrong option entered");
+			break;
+	}
+
+	return count;
+}
+static int gpio_open(struct inode *inode, struct file *file_p)
+{
+	printk(KERN_INFO "gpio_led: gpio_open from = %d:%d\n", imajor(inode), iminor(inode));
+	return 0;
+}
+
+static int gpio_close(struct inode *inode, struct file *file_p)
+{
+	printk(KERN_INFO "gpio_led: gpio_close from = %d:%d\n", imajor(inode), iminor(inode));
+	return 0;
+}
+
+static struct file_operations file_ops =
+{
+	.owner		= THIS_MODULE,
+	.open		= gpio_open,
+	.read		= gpio_read,
+	.write		= gpio_write,
+	.release	= gpio_close,
+};
+
+static int gpio_init(void)
+{
+	int ret;
+	struct device *dev_ret;
+	printk(KERN_INFO "gpio_led: gpio_init +");
+	ret = alloc_chrdev_region(&dev_no, FIRST_MINOR, TOTAL_MINOR, "gpio-led");
+	if (ret < 0)
+	{
+		printk(KERN_ALERT "gpio_led: Device registration failed\n");
+		return ret;
+	}
+	printk("Major Nr: %d\n", MAJOR(dev_no));
+	
+	cdev_init(&char_dev, &file_ops);
+	ret = cdev_add(&char_dev, dev_no, TOTAL_MINOR);
+	if (ret < 0)
+	{
+		printk(KERN_ALERT "gpio_led: Device addition failed\n");
+		unregister_chrdev_region(dev_no, 1);
+		return ret;
+	}
+	
+	//Creatig a class with name gpiodrv. Will create a dir /sys/class/gpio-led
+	class_dev = class_create(THIS_MODULE, "gpio-led");
+	if (IS_ERR(class_dev))
+	{
+		printk(KERN_ALERT "gpio_led: Class creation failed\n");
+		unregister_chrdev_region(dev_no, 1);
+		return PTR_ERR(class_dev);
+	}
+	
+	dev_ret = device_create(class_dev, NULL, MKDEV(MAJOR(dev_no), FIRST_MINOR), NULL, "gpio_led%d", FIRST_MINOR);
+	for(i = FIRST_MINOR + 1; i <= TOTAL_MINOR; i++)
+	{
+		dev_ret = device_create(class_dev, dev_ret, MKDEV(MAJOR(dev_no), i), NULL, "gpio_led%d", i);
+
+		if (IS_ERR(dev_ret))
+		{
+			printk(KERN_ALERT "gpio_led: Device creation failed\n");
+			device_destroy(class_dev, dev_no);
+			class_destroy(class_dev);
+			unregister_chrdev_region(dev_no, TOTAL_MINOR);
+			return PTR_ERR(dev_ret);
+		}
+	}
+	
+
+	char gpio_name[8] = {0,0,0,0,0,0,0,0};
+	for(i = FIRST_MINOR-1; i < TOTAL_MINOR; i++)
+	{
+		printk(KERN_ALERT "gpio_led: i = %d\n", i);
+		ret = gpio_is_valid(gpio_led[i]);
+		if(ret < 0)
+		{
+			printk(KERN_ALERT "gpio_led: gpio %u is not-valid error \n", gpio_led[i]);
+			return -1;
+		}
+
+		sprintf(gpio_name, "LED_%d", i+1);
+		printk(KERN_ALERT "gpio_led: name = %s\n", gpio_name);
+		ret = gpio_request(gpio_led[i], gpio_name);
+		if(ret < 0)
+		{
+			printk(KERN_ALERT "gpio_led: gpio %u request error \n", gpio_led[i]);
+			return -1;
+		}
+		gpio_direction_output(gpio_led[i], 1);
+	}
+	
+	return 0;
+}
+
+void gpio_exit(void)
+{
+	for(i = TOTAL_MINOR; i >= FIRST_MINOR; i--) //Is it should be in that order !
+	{
+		gpio_free(gpio_led[i-1]);
+		device_destroy(class_dev, MKDEV(MAJOR(dev_no), i));
+	}
+	class_destroy(class_dev);
+	cdev_del(&char_dev);
+	unregister_chrdev_region(dev_no, TOTAL_MINOR);
+
+	printk(KERN_INFO "gpio_led: gpio_exit -\n");
+}
+
+module_init(gpio_init);
+module_exit(gpio_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Devesh Nagar <devesh66@gmail.com>");
+MODULE_DESCRIPTION("GPIO LED example");
+
+
+
+/*
+ * If before doing insmod you creat a normal file in /dev/ with same name as that module will create,
+ * You will not able to use that node file /dev/.. Please delete it before loading module.
+ * Or you have to delete, and create mannually using mknod
+ *
+ */
+
+
+/*
+ * int gpio_is_valid(int number);
+ *	number : gpio pin number GPIO_NUMBER.
+ *	gpio_is_valid()
+ *		checks gpio pin number is valid or not
+ */
+
+/*
+ * int gpio_request(unsigned gpio, const char *label);
+ *	gpio : gpio pin number
+ *	label : ?, this is used for debug purpose
+ *			Check cat /sys/kernel/debug/gpio 
+ *
+ */
+/*
+ * void gpio_direction_output (int id, bool value)
+ * 	id : the pin to switch direction
+ *	value : the initial value to drive on the pin (0/1 on/off)
+ *	gpio_direction_output () :
+ *		GPIO pins can be used for input or output. This function make the specified pin an output pin.
+ *		The pin state can the be changed using gpio_set_pin.
+ *		But I am doing pin state change with gpio_set_vapue() !
+ */
---
+++ char-drivers/multiple_minors.c
@@ -0,0 +1,276 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>	/* copy_to_user   */
+#include <linux/device.h>	/* device_destroy */
+#include <linux/cdev.h>		/* cdev_del       */
+#include <linux/slab.h>		/* kfree          */
+
+
+#define FIRST_MINOR 1
+#define TOTAL_MINOR 4
+
+int i;
+
+/*
+ *This variable handles major and minor number of device.
+ */
+static dev_t dev_no;
+static struct cdev char_dev;
+static struct class *final_simple_class;
+
+
+
+static int myOpen(struct inode *i, struct file *f)
+{
+	printk(KERN_INFO "final_simple: myOpen() from = %d:%d\n", imajor(i), iminor(i));
+	return 0;
+}
+static int myClose(struct inode *i, struct file *f)
+{
+	printk(KERN_INFO "final_simple: myClose() +");
+	printk(KERN_INFO "final_simple: myClose() from = %d:%d\n", imajor(i), iminor(i));
+	return 0;
+}
+
+char *sendData;
+static ssize_t myRead(struct file *f, char __user *buf, size_t len, loff_t *off)
+{
+	unsigned int ret = 14;
+	printk(KERN_INFO "final_simple: myReade() from = %d:%d\n", imajor(f->f_inode), iminor(f->f_inode));
+	switch(iminor(f->f_inode))
+	{
+		case FIRST_MINOR:
+			sendData = "Hello First  \n";	//14 bytes size
+			break;
+		case 2:
+			sendData = "Hello Second \n";	//14 bytes size
+			break;
+		case 3:
+			sendData = "Hello Third  \n";	//14 bytes size
+			break;
+		case TOTAL_MINOR:
+			sendData = "Hello Fourth \n";	//14 bytes size
+			break;
+	}
+	//sendData = "Hello World\n"; //12 bytes size
+	if(*off == 0)
+	{
+		ret = copy_to_user(buf, sendData, 14);
+		*off += 14;
+	}	
+	if(ret == 0)
+	{
+		printk(KERN_INFO "final_simple: Copied all to user");
+	}
+	else
+	{
+		printk(KERN_ERR "final_simple: Pending bytes to copy %u", ret);
+	}
+
+//	return 0;
+	return (14-ret);
+/*
+ * On doing cat /dev/final_simple0 :  This function wil be called two times 
+ */
+}
+
+static char* receiveData;
+static ssize_t myWrite(struct file *f, const char __user *buf, size_t len, loff_t *off)
+{
+	ssize_t ret;
+	printk(KERN_INFO "final_simple: myWrite() from = %d:%d\n", imajor(f->f_inode), iminor(f->f_inode));
+	if(len <= 0)
+	{
+		printk(KERN_INFO "final_simple: No data receive, returning");
+		return 0;
+	}
+
+	//receiveData = kmalloc(len, GFP_KERNEL); //non initialize data. while printing, it may print garbage also.
+	receiveData = kzalloc(len, GFP_KERNEL); //initiaize with 0, it will not print garbage while printing.
+
+	ret = copy_from_user(receiveData, buf, len);
+	if(ret > 0)
+	{
+		printk(KERN_ERR "final_simple: Not receive bytes : %u", ret);
+	}
+	
+	printk(KERN_INFO "final_simple: receiveData = %s", receiveData);
+	printk(KERN_INFO "final_simple: after printing receivedData.");
+	kfree(receiveData);
+
+	return (len-ret);
+}
+
+static struct file_operations final_simple_fops =
+{
+	.owner = THIS_MODULE,
+	.open = myOpen,
+	.release = myClose,
+	.read = myRead,
+	.write = myWrite
+};
+
+/*
+ *Init function
+ */
+static int __init myInit(void)
+{
+	int ret;
+	struct device *device_ret;
+
+	ret = alloc_chrdev_region(&dev_no, FIRST_MINOR, TOTAL_MINOR, "final_simple");
+	if (ret < 0)
+	{
+		printk(KERN_ERR "final_simple: final_simple register failed");
+		return ret;
+	}
+	else
+	{
+		printk(KERN_INFO "final_simple: Namaskar, final_simple registered");
+		printk(KERN_INFO "final_simple: final_smiple <Major, Minor>: <%d, %d>\n", MAJOR(dev_no), MINOR(dev_no));
+	}
+
+	cdev_init(&char_dev, &final_simple_fops);
+	ret = cdev_add(&char_dev, dev_no, TOTAL_MINOR);
+	if(ret < 0)
+	{
+		printk(KERN_ERR "final_simple: final_simple cdev_add failed");
+		unregister_chrdev_region(dev_no, TOTAL_MINOR);
+		return ret;
+	}
+
+	final_simple_class = class_create(THIS_MODULE, "char");
+	if(IS_ERR(final_simple_class))
+	{
+		printk(KERN_ERR "final_simple: final_simple class_create failed");
+		cdev_del(&char_dev);
+		unregister_chrdev_region(dev_no, TOTAL_MINOR);
+		return PTR_ERR(final_simple_class);
+	}
+
+	//Create sysfs entry in /sys/class/char and device node in /dev/
+	device_ret = device_create(final_simple_class, NULL, MKDEV(MAJOR(dev_no), FIRST_MINOR), NULL, "final_simple%d", FIRST_MINOR);
+	for(i = (FIRST_MINOR+1); i <= TOTAL_MINOR; i++)
+	{
+		device_create(final_simple_class, device_ret, MKDEV(MAJOR(dev_no), i), NULL, "final_simple%d", (int)i);
+
+		if(IS_ERR(device_ret))
+		{
+			class_destroy(final_simple_class);
+			cdev_del(&char_dev);
+			unregister_chrdev_region(dev_no, TOTAL_MINOR);
+			return PTR_ERR(device_ret);
+		}
+	}
+
+	return 0;
+}
+
+
+/*
+ *Exit function
+ */
+static void __exit myExit(void)
+{
+	for(i = TOTAL_MINOR; i >= FIRST_MINOR; i--)
+	{
+		device_destroy(final_simple_class, MKDEV(MAJOR(dev_no), i) );
+	}
+	class_destroy(final_simple_class);
+	cdev_del(&char_dev);
+	unregister_chrdev_region(dev_no, TOTAL_MINOR);
+	printk(KERN_INFO "final_simple: final_simple unregistered");
+	printk(KERN_INFO "final_simple: after printing exit message");
+}
+
+module_init(myInit);
+module_exit(myExit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Devesh Nagar <devesh66@gmail.com>");
+MODULE_DESCRIPTION("Final Simple Character Driver Template");
+
+
+/*
+ * If you declare vars inbetween code, warning: ISO C90 forbids mixed declarations and code
+ *
+ */
+
+/*
+ * FIRST_MINOR  : First minor number, we can chose any number. Range will wtart from this number.
+ * TOTAL_MINOR  : Total minor numbers required.
+ * dev_no : This variable is used to store major number and minor number.
+ * myInit() : Init function
+ * myExit() : Exit function
+ *
+ */
+
+
+/*
+ *
+ *int alloc_chrdev_region (dev_t* dev, unsigned baseminor, unsigned count, const char* name);
+ *	dev : output parameter for first assigned number 
+ *	baseminor : first of the requested range of minor numbers
+ *	count : the number of minor numbers required
+ *	name : the name of the associated device or driver
+ *	alloc_chrdev_region() :
+ *		Allocates a range of char device numbers.
+ *		The major number will be chosen dynamically, and returned (along with the first minor number) in dev.
+ *		Returns zero or a negative error code. 
+ */
+  
+/*
+ *void unregister_chrdev_region (dev_t from, unsigned count);
+ *	from : the first in the range of numbers to unregister 
+ *	count: the number of device numbers to unregister
+ *	unregister_chrdev_region() :
+ *		This function will unregister a range of count device numbers, starting with from.
+ *		The caller should normally be the one who allocated those numbers in the first place.
+ */
+
+/*
+ *void cdev_init (struct cdev* cdev, const struct file_operations* fops);
+ *	cdev : the structure to initialize
+ *	fops : the file_operations for this device
+ *	cdev_init() :
+ *		Initializes cdev, remembering fops, making it ready to add to the system with cdev_add.
+ */
+
+/*
+ *int cdev_add (struct cdev* p, dev_t dev, unsigned count);
+ *	p   : the cdev structure for the device
+ *	dev : the first device number for which this device is responsible
+ *	count : the number of consecutive minor numbers corresponding to this device
+ *	cdev_add() :
+ *		cdev_add adds the device represented by p to the system, making it live immediately.
+ *		A negative error code is returned on failure.
+ */
+
+/*
+ *struct class * class_create (struct module* owner, const char* name);
+ *	owner : pointer to the module that is to “own” this struct class
+ *	name  : pointer to a string for the name of this class
+ *	class_create() :
+ *		This is used to create a struct class pointer that can then be used in calls to class_device_create.
+ *		Note, the pointer created here is to be destroyed when finished by making a call to class_destroy.
+ */
+
+/*
+ *struct device* device_create(struct class* class, struct device* parent, dev_t devt, const char* fmt, ...);
+ *	class  : pointer to the struct class that this device should be registered to
+ *	parent : pointer to the parent struct device of this new device, if any
+ *	devt : the dev_t for the char device to be added
+ *	fmt  : string for the device's name
+ *	device_create() :
+ *		This function can be used by char device classes. A struct device will be created in sysfs, registered to the specified class.
+ *		A “dev” file will be created, showing the dev_t for the device, if the dev_t is not 0,0.
+ *		If a pointer to a parent struct device is passed in, the newly created struct device will be a child of that device in sysfs.
+ *		The pointer to the struct device will be returned from the call.
+ *		Any further sysfs files that might be required can be created using this pointer.
+ *		The struct class passed to this function must have previously been created with a call to class_create.
+ *
+ */
